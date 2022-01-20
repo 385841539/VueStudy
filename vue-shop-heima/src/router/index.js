7e7368f0a63da0133d2b1e07bb9fe7354b48eebe
丁 <dingrui@shizhuang-inc.com>
2022-01-20 19:44:11 +0800

    feat:login

--- vue-shop-heima/src/router/index.js
+++ vue-shop-heima/src/router/index.js
@@ -1,27 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
+
 ]
 
 const router = new VueRouter({
   routes
+})
+
+
+
+// 路由导航首位
+router.beforeEach((to, from, next) => {
+
+
+  if (to.path === '/login') return next()
+  var token = localStorage.getItem("token");
+  console.log(token)
+  console.log(to.path)
+  console.log("-----111-")
+
+
+  if (token) {
+    console.log("------")
+    next()
+  } else {
+    return next("/login");
+  }
+
 })
 
 export default router
