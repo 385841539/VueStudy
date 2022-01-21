46f2e6f924523c1cf2daa431c45d77af1caf8b46
丁 <dingrui@shizhuang-inc.com>
2022-01-21 18:32:46 +0800

    feat:用户信息

--- vue-shop-heima/src/main.js
+++ vue-shop-heima/src/main.js
@@ -6,29 +6,13 @@ import 'element-ui/lib/theme-chalk/index.css'
 import less from 'less'
 import './assets/css/global.css'
 import './plugins/element'
+import './plugins/http'
 //导入字体图标
 import './assets/fonts/iconfont.css'
 
-import axios from 'axios'
 
 
-//获取域名
-var host = window.location.host;
-var host2 = document.domain;
 
-//获取页面完整地址
-var url = window.location.href;
-
-
-console.log("url")
-console.log(url)
-console.log("host")
-console.log(host)
-console.log("host2")
-console.log(host2)
-
-axios.defaults.baseURL = "http://" + host + "/api/privata/v1/"
-Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(less)
 
